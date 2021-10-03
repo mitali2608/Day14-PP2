686c902f647259a532049f50776e519a0bd8bc61
mitali2608 <mitalibawankule21@gmail.com>
2021-10-03 23:09:05 +0530

    Added class UC1UC2

---
+++ UC1UC2Stack.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14_PP2
+{
+    class UC1UC2Stack
+    {
+        private Node top;
+
+        public void LinkedListStack()
+        {
+            this.top = null;
+        }
+
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+                node.next = null;
+            else
+                node.next = this.top;
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack ", value);
+        }
+
+      
+        internal void Peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine("{0} is in the top of the stack ", this.top.data);
+        }
+
+        internal void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty, Deletion is not possible");
+                return;
+            }
+            Console.WriteLine("Value popped is {0} ", this.top.data);
+            this.top = this.top.next;
+        }
+
+        internal void EmptyStack()
+        {
+            while (this.top != null)
+            {
+                Peek();
+                Pop();
+            }
+            this.top = null;
+        }
+
+        public void Printstack()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
