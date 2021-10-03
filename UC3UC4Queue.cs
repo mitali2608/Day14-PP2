0e375236e8064bc2a0fd7615855c074df1d51211
mitali2608 <mitalibawankule21@gmail.com>
2021-10-03 23:09:58 +0530

    Added class UC3UC4

---
+++ UC3UC4Queue.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14_PP2
+{
+    class UC3UC4Queue
+    {
+        Node head, tail;
+
+        public void Queue()
+        {
+            this.head = this.tail = null;
+        }
+
+        public void enqueue(int key)
+        {
+
+
+            Node temp = new Node(key);
+
+
+            if (this.tail == null)
+            {
+                this.head = this.tail = temp;
+                return;
+            }
+
+
+            this.tail.next = temp;
+            this.tail = temp;
+
+        }
+
+
+        public void dequeue()
+        {
+
+            if (this.head == null)
+                return;
+
+
+            Node temp = this.head;
+            this.head = this.head.next;
+
+
+            if (this.head == null)
+                this.tail = null;
+            Console.WriteLine("Head is removed");
+        }
+
+        internal void PrintQueue()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+
+
+        }
+
+    }
+}
